566d3f8d38a051839425dde14c74a93fbe527059
mygit <huangzhaoyang7705@163.com>
2020-06-16 22:06:53 +0800

    第二次

--- changgou-service-api/changgou-service-goods-api/src/main/java/com/changgou/goods/pojo/Brand.java
+++ changgou-service-api/changgou-service-goods-api/src/main/java/com/changgou/goods/pojo/Brand.java
@@ -1,68 +1,80 @@
 package com.changgou.goods.pojo;
 
-import io.swagger.annotations.ApiModelProperty;
-
 import javax.persistence.*;
 import java.io.Serializable;
 
+/****
+ * @Author:admin
+ * @Description:Brand构建
+ * @Date 2019/6/14 19:13
+ *****/
 @Table(name="tb_brand")
-public class Brand implements Serializable {
-    @ApiModelProperty
-    @Id
+public class Brand implements Serializable{
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id; //品牌id
-    @ApiModelProperty(value = "品牌名称",required = false)
+	private Integer id;//品牌id
+
     @Column(name = "name")
-    private String name;
-    @ApiModelProperty(value = "品牌图片地址",required = false)
+	private String name;//品牌名称
+
     @Column(name = "image")
-    private String image;
-    @ApiModelProperty(value = "品牌的首字母",required = false)
+	private String image;//品牌图片地址
+
     @Column(name = "letter")
-    private String letter;
+	private String letter;//品牌的首字母
 
-    @ApiModelProperty(value = "排序",required = false)
     @Column(name = "seq")
-    private Integer seq;
+	private Integer seq;//排序
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
+	//get方法
+	public Integer getId() {
+		return id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	//set方法
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	//get方法
+	public String getName() {
+		return name;
+	}
 
-    public String getImage() {
-        return image;
-    }
+	//set方法
+	public void setName(String name) {
+		this.name = name;
+	}
+	//get方法
+	public String getImage() {
+		return image;
+	}
 
-    public void setImage(String image) {
-        this.image = image;
-    }
+	//set方法
+	public void setImage(String image) {
+		this.image = image;
+	}
+	//get方法
+	public String getLetter() {
+		return letter;
+	}
 
-    public String getLetter() {
-        return letter;
-    }
+	//set方法
+	public void setLetter(String letter) {
+		this.letter = letter;
+	}
+	//get方法
+	public Integer getSeq() {
+		return seq;
+	}
 
-    public void setLetter(String letter) {
-        this.letter = letter;
-    }
+	//set方法
+	public void setSeq(Integer seq) {
+		this.seq = seq;
+	}
 
-    public Integer getSeq() {
-        return seq;
-    }
 
-    public void setSeq(Integer seq) {
-        this.seq = seq;
-    }
 }
--- changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -1,16 +1,26 @@
 package com.changgou;
 
+import entity.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan(basePackages = {"com.changgou.dao"}) //开启通用Mapper的包扫描
+@MapperScan(basePackages = {"com.changgou.*.dao"}) //开启通用Mapper的包扫描
+@EnableSwagger2
+@EnableCircuitBreaker
 public class GoodsApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GoodsApplication.class,args);
     }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(0,1) ;}
 }
